a669a9c3ffcefff4f32d8cccb013b0d0730530f3
pchq <9292358@gmail.com>
2019-03-05 22:36:35 +0800

    Updates

---
+++ precache-manifest.9b8e14015beb019027b6e44f7c97f8f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7bd7dd416eb9efe0fd9d",
+    "url": "/simple-todo/static/js/runtime~main.7bd7dd41.js"
+  },
+  {
+    "revision": "ec36a8fa6ca31e089ad3",
+    "url": "/simple-todo/static/js/main.ec36a8fa.chunk.js"
+  },
+  {
+    "revision": "86fd2c6431b26deabcf5",
+    "url": "/simple-todo/static/js/1.86fd2c64.chunk.js"
+  },
+  {
+    "revision": "ec36a8fa6ca31e089ad3",
+    "url": "/simple-todo/static/css/main.4c9c007e.chunk.css"
+  },
+  {
+    "revision": "71e5b297d32eaa554db06541be5f1f71",
+    "url": "/simple-todo/index.html"
+  }
+];
